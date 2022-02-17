6e24014c06189cc1222d7325d4d28b97bba0f293
Ehtisham Ahmed <ehtishama876@gmail.com>
2022-02-17 18:08:50 +0500

    v2 is completed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 // import './Todo.css';
 
 
-function Task({ task, index, completeTask, removeTask }) {
+function Task({ task, index, completeTask, removeTask, setEditing }) {
+    const inputRef = useRef(null);
+    const onCheck = ()=>{
+        console.log(inputRef.current.value);
+        setEditing(index, false, inputRef.current.value)
+    }
     return (
         <div
             className=" flex border-2 rounded w-full border-white bg-gray-100 p-2 m-2"
-            style={{ textDecoration: task.completed ? "line-through" : "" }}
-        >
-           <span className='flex-1'> {task.title}</span>
+        >   
+            {
+                task.editing 
+                ? 
+                <div className='flex-1 flex pr-4'>
+                    <input ref={inputRef} className='flex-1' defaultValue={task.title}/>
+                    <button className='bg-green-400 px-5' onClick={onCheck}>✔︎</button>
+                </div>
+                : <span className='flex-1' style={{ textDecoration: task.completed ? "line-through" : "" }}> {task.title}</span>
+            }
           
             <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red  hover:bg-red m-2 " onClick={() => removeTask(index)}>x</button>
             <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:bg-teal " onClick={() => completeTask(index)}>Complete</button>
-          
+            {
+                !task.editing && 
+                <button className="ml-2 flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:bg-teal " onClick={() => setEditing(index, true)}>Edit</button>
+            }     
         </div>
     );
 }
@@ -45,20 +60,41 @@ function Main() {
     const [tasks, setTasks] = useState([
         {
             title: "Grab some Pizza",
-            completed: true
+            completed: true,
+            editing: false
         },
         {
             title: "Do your workout",
-            completed: true
+            completed: true,
+            editing: false
         },
         {
             title: "Hangout with friends",
-            completed: false
+            completed: false,
+            editing: false
         }
     ]);
 
     useEffect(() => { setTasksRemaining(tasks.filter(task => !task.completed).length) });
 
+    const setEditing = (ind, value, text) => {
+        setTasks((oldState)=>{
+            const newState = oldState.map((item, index)=>{
+                if(ind === index){
+                    return {
+                        ...item, editing: value ?? false,
+                        title: !!text ? text : item.title
+                    }
+                }else{
+                    return item
+                }
+            })
+           
+            
+            return newState
+        })
+
+    }
 
     const addTask = title => {
         const newTasks = [...tasks, { title, completed: false }];
@@ -78,8 +114,10 @@ function Main() {
     };
 
     return (
-        <div className='bg-blue-400 p-4 m-10'>
-            <h1 className="text-black font-bold  ">Pending tasks ({tasksRemaining})</h1>
+        <div className='bg-blue-400 p-4 m-10 '>
+            <div className="m-2" >
+                <CreateTask addTask={addTask} />
+            </div>
             <div className="border rounded w-auto py-2 px-3 mr-4 bg-green-500 text-grey-darker">
                 {tasks.map((task, index) => (
                     <Task
@@ -87,13 +125,14 @@ function Main() {
                     index={index}
                     completeTask={completeTask}
                     removeTask={removeTask}
+                    setEditing={setEditing}
                     key={index}
                     />
                 ))}
             </div>
-            <div className="create-task" >
-                <CreateTask addTask={addTask} />
-            </div>
+            <h1 className="text-black font-bold  ">Pending tasks ({tasksRemaining})</h1>
+
+            
         </div>
     );
 }
